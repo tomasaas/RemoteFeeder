8ed8739e5f087aa5c5334da8e541b07b2cfacc6a
Tomas Aas-Hansen <tomasa@uia.no>
2026-08-22 21:46:28 +0200

    Added hardcoded WiFi connection.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <ESP32Servo.h>
 #include "feed.h"
+#include <WiFi.h>
 
 Servo myservo;
+
+const char* ssid = "Zyxel_8086";
+const char* password = "REMOVED";
 
 void setup() 
 {
@@ -10,10 +14,21 @@ void setup()
     myservo.attach(13, 1000, 2000);  // Attach to pin 18, min pulsbredde (1500 senturm vinkel), maks pulsbredde
     bool attached = myservo.attached();
     attached ? Serial.println("Attached successfully to myservo!") : Serial.println("Attachment to myservo failed");
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) { 
+        delay(500); 
+    }
+    Serial.println("Wi-Fi connected successfully!");
+    WiFi.mode(WIFI_STA); // vanlig wifi klient, bare for ordens skyld. 
+
+
 }
 
 void loop() 
 {
-    feed(myservo);
+    // feed(myservo);
+
+
 }
 
